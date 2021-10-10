71f78631cc05eea962b88a0a62b8d884e829eec9
Thomas Hansen <thomas@gaiasoul.com>
2021-10-10 14:25:35 +0300

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+cd src
+
+cd magic.lambda
+git pull 
+cd ..
+
+cd magic.endpoint
+git pull 
+cd ..
+
+cd magic.crypto
+git pull 
+cd ..
+
+cd magic.lambda.system
+git pull 
+cd ..
+
+cd magic.http
+git pull 
+cd ..
+
+cd magic.io
+git pull 
+cd ..
+
+cd magic.library
+git pull 
+cd ..
+
+cd magic.node
+git pull 
+cd ..
+
+cd magic.signals
+git pull 
+cd ..
+
+cd magic.data.common
+git pull 
+cd ..
+
+cd magic.lambda.auth
+git pull 
+cd ..
+
+cd magic.lambda.sockets
+git pull 
+cd ..
+
+cd magic.lambda.ad-auth
+git pull 
+cd ..
+
+cd magic.lambda.guid
+git pull 
+cd ..
+
+cd magic.lambda.config
+git pull 
+cd ..
+
+cd magic.lambda.crypto
+git pull 
+cd ..
+
+cd magic.lambda.dates
+git pull 
+cd ..
+
+cd magic.lambda.http
+git pull 
+cd ..
+
+cd magic.lambda.hyperlambda
+git pull 
+cd ..
+
+cd magic.lambda.io
+git pull 
+cd ..
+
+cd magic.lambda.json
+git pull 
+cd ..
+
+cd magic.lambda.html
+git pull 
+cd ..
+
+cd magic.lambda.csv
+git pull 
+cd ..
+
+cd magic.lambda.logging
+git pull 
+cd ..
+
+cd magic.lambda.image
+git pull 
+cd ..
+
+cd magic.lambda.caching
+git pull 
+cd ..
+
+cd magic.lambda.mail
+git pull 
+cd ..
+
+cd magic.lambda.math
+git pull 
+cd ..
+
+cd magic.lambda.mime
+git pull 
+cd ..
+
+cd magic.lambda.mssql
+git pull 
+cd ..
+
+cd magic.lambda.mysql
+git pull 
+cd ..
+
+cd magic.lambda.scheduler
+git pull 
+cd ..
+
+cd magic.lambda.slots
+git pull 
+cd ..
+
+cd magic.lambda.strings
+git pull 
+cd ..
+
+cd magic.lambda.validators
+git pull 
+cd ..
