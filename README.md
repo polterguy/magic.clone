32f1b220392410e6567664e39ec9049b97926d86
Thomas Hansen <thomas@gaiasoul.com>
2021-10-10 14:03:25 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# magic.clone
+
+# Magic Clone - Helper repository to clone and debug all Magic code locally
+
+This repository serves one simple purpose, which is to allow you to rapidly clone and be able
+to debug all plugins in Magic.
+
+Clone this repository locally, then run the _"clone.sh"_ file, and wait until it's done - At
+which point you will find everything needed to run and debug Magic locally within your _"src/"_
+folder.
---
+++ clone.sh
@@ -0,0 +1,42 @@
+
+#!/bin/bash
+
+mkdir ./src
+cd src
+
+git clone https://github.com/polterguy/magic.magic.lambda.git
+git clone https://github.com/polterguy/magic.magic.endpoint.git
+git clone https://github.com/polterguy/magic.magic.crypto.git
+git clone https://github.com/polterguy/magic.magic.lambda.system.git
+git clone https://github.com/polterguy/magic.magic.http.git
+git clone https://github.com/polterguy/magic.magic.io.git
+git clone https://github.com/polterguy/magic.magic.library.git
+git clone https://github.com/polterguy/magic.magic.node.git
+git clone https://github.com/polterguy/magic.magic.signals.git
+git clone https://github.com/polterguy/magic.magic.data.common.git
+git clone https://github.com/polterguy/magic.magic.lambda.auth.git
+git clone https://github.com/polterguy/magic.magic.lambda.sockets.git
+git clone https://github.com/polterguy/magic.magic.lambda.ad-auth.git
+git clone https://github.com/polterguy/magic.magic.lambda.guid.git
+git clone https://github.com/polterguy/magic.magic.lambda.config.git
+git clone https://github.com/polterguy/magic.magic.lambda.crypto.git
+git clone https://github.com/polterguy/magic.magic.lambda.dates.git
+git clone https://github.com/polterguy/magic.magic.lambda.http.git
+git clone https://github.com/polterguy/magic.magic.lambda.hyperlambda.git
+git clone https://github.com/polterguy/magic.magic.lambda.io.git
+git clone https://github.com/polterguy/magic.magic.lambda.json.git
+git clone https://github.com/polterguy/magic.magic.lambda.html.git
+git clone https://github.com/polterguy/magic.magic.lambda.csv.git
+git clone https://github.com/polterguy/magic.magic.lambda.logging.git
+git clone https://github.com/polterguy/magic.magic.lambda.image.git
+git clone https://github.com/polterguy/magic.magic.lambda.caching.git
+git clone https://github.com/polterguy/magic.magic.lambda.mail.git
+git clone https://github.com/polterguy/magic.magic.lambda.math.git
+git clone https://github.com/polterguy/magic.magic.lambda.mime.git
+git clone https://github.com/polterguy/magic.magic.lambda.mssql.git
+git clone https://github.com/polterguy/magic.magic.lambda.mysql.git
+git clone https://github.com/polterguy/magic.magic.lambda.scheduler.git
+git clone https://github.com/polterguy/magic.magic.lambda.slots.git
+git clone https://github.com/polterguy/magic.magic.lambda.strings.git
+git clone https://github.com/polterguy/magic.magic.lambda.validators.git
+git clone https://github.com/polterguy/magic.git
